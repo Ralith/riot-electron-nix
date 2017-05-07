9661ad7f2c26c7ec1499d6a578c59bf76bf92e2a
Benjamin Saunders <ben.e.saunders@gmail.com>
2017-05-06 17:36:56 -0700

    0.9.7 -> 0.9.8

--- default.nix
+++ default.nix
@@ -2,18 +2,18 @@
 let
 riot = stdenv.mkDerivation rec {
   name = "riot-desktop-${version}";
-  version = "0.9.7";
+  version = "0.9.8";
 
   src = fetchFromGitHub {
     owner = "vector-im";
     repo = "riot-web";
     rev = "v${version}";
-    sha256 = "0qcf8g8mvvjgmg79mdk7a2b7g2jw6py4ws087ir79y3z3k7zfs34";
+    sha256 = "0j5jdkvxic912hfbvh63l5f2rqad8vvp0bjdq9fbspp5gdlc0w39";
   };
 
   packaged = fetchurl {
     url = "https://github.com/vector-im/riot-web/releases/download/v${version}/vector-v${version}.tar.gz";
-    sha256 = "14x6ndrnvb50qhmw43yhdrk25jqh7ac7ym3g0gf5zzr609455717";
+    sha256 = "0rrqdhr81f7kz9qcwhi5f5n7ng21c6zcn53xvygcr401pv69057f";
   };
 
   buildInputs = [ gnutar ];
