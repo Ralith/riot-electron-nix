ad6ae1df01c20308f3879930d9909008a826ae31
Benjamin Saunders <ben.e.saunders@gmail.com>
2017-11-20 20:14:31 -0800

    0.12.7 -> 0.13.1 and build from source

--- default.nix
+++ default.nix
@@ -1,24 +1,25 @@
-{ stdenv, fetchurl, runCommand, writeScriptBin, electron, bash, gnutar, pkgs, system, nodejs }:
+{ stdenv, fetchurl, runCommand, writeScriptBin, electron, bash, pkgs, system, nodejs }:
 let
 nodePackages = import ./node { inherit pkgs system nodejs; };
+riot-web = nodePackages."riot-web-file:../riot-web".override (old: {
+  postInstall = ''
+    echo "building..."
+    npm run build
+  '';
+});
 riot = stdenv.mkDerivation rec {
   name = "riot-desktop-${version}";
-  version = "0.12.7";
-
-  src = fetchurl {
-    url = "https://github.com/vector-im/riot-web/releases/download/v${version}/riot-v${version}.tar.gz";
-    sha256 = "19r3jrrs07wmzd5gc5f68ldsn3v1zrlvqywqmly74j9pd6h3pk3l";
-  };
-
-  nativeBuildInputs = [ gnutar ];
+  version = "0.13.1";
 
   buildCommand = ''
-    mkdir -p "$out/webapp"
-    tar xf '${src}' --strip-components=1 -C "$out/webapp"
+    mkdir -p "$out"
+    cp -r '${riot-web}/lib/node_modules/riot-web/webapp' "$out/webapp"
+    chmod +w "$out/webapp"
+    echo '${version}' > "$out/webapp/version"
     cp -r '${nodePackages."riot-web-file:../riot-web/electron_app"}/lib/node_modules/riot-web/' "$out/electron"
   '';
 };
 in writeScriptBin "riot" ''
   #!${bash}/bin/sh
-  ${electron}/bin/electron "${riot}/electron" "$@"
+  "${electron}/bin/electron" "${riot}/electron" "$@"
 ''
