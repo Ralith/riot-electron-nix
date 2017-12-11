91ac89c6a28bb4d35fba0dcf38f2666567e4332f
Ruben Maher <ruben@maher.fyi>
2017-12-12 09:20:29 +1030

    Use package-lock.json and pin revision of node2nix

--- node/generate.sh
+++ node/generate.sh
@@ -1,4 +1,10 @@
 #!/bin/sh -e
 
 rm -f default.nix node-packages.nix node-env.nix
-node2nix -d -6 -i node-packages.json -c default.nix -e node-env.nix
+"$(nix-build ./node2nix.nix --no-out-link)/bin/node2nix" \
+  --development \
+  --nodejs-6 \
+  --input node-packages.json \
+  --composition default.nix \
+  --node-env node-env.nix \
+  --lock ../riot-web/package-lock.json
