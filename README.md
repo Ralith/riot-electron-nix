57053acb2c59eb67edd690a0a27359de5c5d5cc9
Benjamin Saunders <ben.e.saunders@gmail.com>
2017-12-11 16:20:59 -0800

    Cleanup
    - Remove outdated README info
    - Use makeDesktopItem
    - More FHS-y directory layout
    - Simplify update procedure

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
-This package is unsuitable for upstreaming into nixpkgs due to the somewhat esoteric build process. It could still
-conceivably be improved by building riot-web from source, thanks to node2nix.
-
-Riot is incompatible with the version of electron packaged in the `nixos-17.03` channel. The package currently must be
-built against `nixos-unstable`.
-
-If this package is defined as `riot` is defined in your packageOverrides, it can be installed against unstable with
-`nix-env -f https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz -iA riot`
-
-To update to a new version, update the riot-web submodule and run `./generate.sh` in the node
-subdirectory, then update the root default.nix as usual.
+To update to a new version, update the riot-web submodule and run `./generate.sh` in the node subdirectory.
--- default.nix
+++ default.nix
@@ -1,46 +1,46 @@
-{ stdenv, electron, pkgs, system, nodejs }:
+{ stdenv, electron, pkgs, system, nodejs, makeDesktopItem }:
 let
 nodePackages = import ./node { inherit pkgs system nodejs; };
-riot-web = nodePackages."riot-web-file:../riot-web".override (old: {
+riot-web = nodePackages."riot-web-file:../riot-web".override (attrs: {
   postInstall = ''
     echo "building..."
     npm run build
+    echo '${attrs.version}' > "webapp/version"
   '';
 });
-in stdenv.mkDerivation rec {
-  name = "riot-desktop-${version}";
-  version = "0.13.3";
+desktopItem = makeDesktopItem {
+  name = "riot";
+  desktopName = "Riot";
+  genericName = "Matrix Client";
+  exec = "riot";
+  comment = "A feature-rich client for Matrix.org";
+  icon = "riot";
+  categories = "Network;InstantMessaging;Chat;";
+  extraEntries = ''
+    StartupWMClass="Riot"
+  '';
+};
+in stdenv.mkDerivation {
+  name = "riot-desktop-${riot-web.version}";
+  inherit (riot-web) version;
 
   buildCommand = ''
-    mkdir -p "$out"
-    cp -r '${riot-web}/lib/node_modules/riot-web/webapp' "$out/webapp"
-    chmod +w "$out/webapp"
-    echo '${version}' > "$out/webapp/version"
-    cp -r '${nodePackages."riot-web-file:../riot-web/electron_app"}/lib/node_modules/riot-web/' "$out/electron"
+    mkdir -p "$out/share/riot"
+    ln -s '${riot-web}/lib/node_modules/riot-web/webapp' "$out/share/riot/webapp"
+    cp -r '${nodePackages."riot-web-file:../riot-web/electron_app"}/lib/node_modules/riot-web/' "$out/share/riot/electron"
 
     for i in 16 24 48 64 96 128 256 512; do
-      install -Dm644 $out/electron/build/icons/''${i}x''${i}.png \
-        $out/share/icons/hicolor/''${i}x''${i}/apps/riot.png
+      mkdir -p "$out/share/icons/hicolor/''${i}x''${i}/apps"
+      ln -s "$out/share/riot/electron/build/icons/''${i}x''${i}.png" \
+        "$out/share/icons/hicolor/''${i}x''${i}/apps/riot.png"
     done
 
-    mkdir -p $out/share/applications
-    cat > $out/share/applications/riot.desktop <<EOF
-    [Desktop Entry]
-    Name=Riot
-    Comment=A feature-rich client for Matrix.org
-    Exec=riot
-    Terminal=false
-    Type=Application
-    Icon=riot
-    StartupWMClass="Riot"
-    Categories=Network;InstantMessaging;Chat
-    EOF
-
-    mkdir -p $out/bin
-    cat > $out/bin/riot <<EOF
+    cp -r '${desktopItem}/.' "$out"
+    mkdir -p "$out/bin"
+    cat > "$out/bin/riot" <<EOF
     #!${stdenv.shell}
-    ${electron}/bin/electron "$out/electron" "$@"
+    '${electron}/bin/electron' "$out/share/riot/electron" "$@"
     EOF
-    chmod +x $out/bin/riot
+    chmod +x "$out/bin/riot"
   '';
 }
